b37989dcde742129c652739e9c9b63f50757cdbe
Patryk Przybek <patryk.przybek@interia.pl>
2017-11-04 13:49:52 +0100

    mial reacje

--- src/pl/webps/ProjektPierwszy/ProjektPierwszy.java
+++ src/pl/webps/ProjektPierwszy/ProjektPierwszy.java
@@ -5,5 +5,6 @@ public class ProjektPierwszy {
 	public static void main(String [] args ){
 		System.out.println("Hello World");		
 		System.out.println("druga linia");
+		System.out.println("Konfikt");
 	}
 }
